e72fbbf2a388c1011c9fbfce4e2c36d5eb33af2c
Raghavendra <raghava.u@gmail.com>
2016-01-06 11:37:20 +0530

    added new files

---
+++ App_Code/Connection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for Connection
+/// </summary>
+public class Connection
+{
+    public Connection()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public SqlConnection SCon=new SqlConnection("Data Source=172.168.3.122;Initial Catalog=rfid;User ID=sa;Password=123456");
+
+    public bool OpenSqlConnection()
+    {
+        bool Status=false;
+        try 
+        {
+            SCon=new SqlConnection("Data Source=172.168.3.122;Initial Catalog=rfid;User ID=sa;Password=123456");
+            SCon.Open();
+            Status=true;
+        }
+        catch(Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+        finally
+        { SCon.Close(); }
+        return Status;
+    }
+}
---
+++ App_Code/UtilityClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+/// <summary>
+/// Summary description for UtilityClass
+/// </summary>
+public class UtilityClass
+{
+    public UtilityClass()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
